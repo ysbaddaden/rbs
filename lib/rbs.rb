4c9209b683b7b1e8ab7687dddd6a468b9da2871a
Julien Portalier <julien@portalier.com>
2015-04-21 16:24:21 +0200

    RBS.compile(source) and RBS.compile_file(path)

--- lib/rbs.rb
+++ lib/rbs.rb
@@ -5,3 +5,21 @@ require "rbs/parser"
 require "rbs/parser/rewriter"
 require "rbs/formatter"
 require "rbs/indenter"
+
+module RBS
+  def self.compile_file(path, options = {})
+    compile(File.read(path), options)
+  end
+
+  def self.compile(source, indent: true, **options)
+    lexer = RBS::Rewriter.new(RBS::Lexer.new(source))
+    parser = RBS::Parser::Rewriter.new(RBS::Parser.new(lexer))
+
+    formatter = if indent
+                  RBS::Indenter.new(RBS::Formatter.new(parser))
+                else
+                  RBS::Formatter.new(parser)
+                end
+    formatter.compile(options)
+  end
+end
