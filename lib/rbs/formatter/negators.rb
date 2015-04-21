fea0f56689ec0f0fecc76c4fc2e682bebc3da847
Julien Portalier <julien@portalier.com>
2015-04-21 23:31:40 +0200

    Simplify RBS::Negators

--- lib/rbs/formatter/negators.rb
+++ lib/rbs/formatter/negators.rb
@@ -6,10 +6,8 @@ module RBS
     def self.simple_negate(node)
       if negation?(node)
         node.argument
-      elsif node === %i(identifier literal group_expression)
-        Node.new(:unary_expression, operator: "!", argument: node)
       else
-        Node.new(:unary_expression, operator: "!", argument: Node.new(:group_expression, expression: node))
+        common_negate(node)
       end
     end
 
@@ -26,38 +24,38 @@ module RBS
         negate_logical_expression(node)
       elsif negatable?(node)
         negate_binary_expression(node)
-      elsif node === %i(identifier literal group_expression)
+      else
+        common_negate(node)
+      end
+    end
+
+    private
+
+    def self.common_negate(node)
+      if node === %i(identifier literal group_expression)
         Node.new(:unary_expression, operator: "!", argument: node)
       else
         Node.new(:unary_expression, operator: "!", argument: Node.new(:group_expression, expression: node))
       end
     end
 
-    private
-
     def self.negate_logical_expression(node)
-      left = if negation?(node.left)
-               node.left.argument
-             elsif logical?(node.left)
-               negate_logical_expression(node.left)
-             elsif negatable?(node.left)
-               negate_binary_expression(node.left)
-             else
-               Node.new(:unary_expression, operator: "!", argument: node.left)
-             end
-
-      right = if negation?(node.right)
-                node.right.argument
-              elsif logical?(node.right)
-                negate_logical_expression(node.right)
-              elsif negatable?(node.right)
-                negate_binary_expression(node.right)
-              else
-                Node.new(:unary_expression, operator: "!", argument: node.right)
-              end
-
+      left = negate(node.left)
+      right = negate(node.right)
       operator = node.operator == "&&" ? "||" : "&&"
       Node.new(:binary_expression, operator: operator, left: left, right: right)
+    end
+
+    def self.negate(node)
+      if negation?(node)
+        node.argument
+      elsif logical?(node)
+        negate_logical_expression(node)
+      elsif negatable?(node)
+        negate_binary_expression(node)
+      else
+        Node.new(:unary_expression, operator: "!", argument: node)
+      end
     end
 
     def self.negate_binary_expression(node)
